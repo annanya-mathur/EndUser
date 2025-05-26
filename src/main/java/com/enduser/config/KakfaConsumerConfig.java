8c1be97addec6881f8e2d77fc61583f5d466c50c
annanya-mathur <annanyasmathur2017@gmail.com>
2025-05-26 18:39:01 +0530

    updating end user

--- src/main/java/com/enduser/config/KakfaConsumerConfig.java
+++ src/main/java/com/enduser/config/KakfaConsumerConfig.java
@@ -1,15 +1,17 @@
 package com.enduser.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.KafkaListener;
 
 import static com.enduser.config.AppConstant.GROUP_ID;
 import static com.enduser.config.AppConstant.LOCATION_TOPIC_NAME;
 
+@Configuration
 public class KakfaConsumerConfig {
 
     @KafkaListener(topics = LOCATION_TOPIC_NAME , groupId = GROUP_ID)
     public void updatedLocation(String location)
     {
-
+             System.out.println(location);
     }
 }
