9ac9305d695b3a1421ab1dd7a3c0c2cd4077edb3
annanya-mathur <annanyasmathur2017@gmail.com>
2025-05-27 14:25:56 +0530

    eliminating warnings

--- src/main/java/com/enduser/config/KafkaConsumerConfig.java
+++ src/main/java/com/enduser/config/KafkaConsumerConfig.java
@@ -7,7 +7,7 @@ import static com.enduser.config.AppConstant.GROUP_ID;
 import static com.enduser.config.AppConstant.LOCATION_TOPIC_NAME;
 
 @Configuration
-public class KakfaConsumerConfig {
+public class KafkaConsumerConfig {
 
     @KafkaListener(topics = LOCATION_TOPIC_NAME , groupId = GROUP_ID)
     public void updatedLocation(String location)
